8960d6bbc16f4f962b083381fcb7c69983d49c1a
mc-ark1 <markt@m-markz.com>
2024-02-02 11:06:56 +0900

    fix: config

--- colors/anemone.lua
+++ colors/anemone.lua
@@ -15,7 +15,7 @@ vim.g.colors_name = "anemone"
 -- The performance impact of this call can be measured in the hundreds of
 -- *nanoseconds* and such could be considered "production safe".
 -- package.loaded["lush_theme.anemone"] = nil
-package.loaded["anemone"] = nil
+-- package.loaded["anemone"] = nil
 
 -- include our theme file and pass it to lush to apply
-require("lush")(require("lush_theme.anemone"))
+-- require("lush")(require("lush_theme.anemone"))
